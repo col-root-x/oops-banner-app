7823d230d84a910dd22be4b51ec73f46ac693a78
Ankit Rishiraaj <ankitrishir@gmail.com>
2026-03-03 09:34:46 +0530

    UC8: Use HashMap for efficient character pattern retrieval

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,89 +1,58 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OOPSBannerApp {
 
-    // Static Inner Class
-    static class CharacterPattern {
-        private char letter;
-        private String[] pattern;
-
-        public CharacterPattern(char letter, String[] pattern) {
-            this.letter = letter;
-            this.pattern = pattern;
-        }
-
-        public char getLetter() {
-            return letter;
-        }
-
-        public String[] getPattern() {
-            return pattern;
-        }
-    }
-
     public static void main(String[] args) {
 
-        List<CharacterPattern> patternList = new ArrayList<>();
+        Map<Character, String[]> patternMap = new HashMap<>();
 
-        patternList.add(new CharacterPattern('O', getO()));
-        patternList.add(new CharacterPattern('P', getP()));
-        patternList.add(new CharacterPattern('S', getS()));
+        patternMap.put('O', new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****"
+        });
 
-        printWord("OOPS", patternList);
+        patternMap.put('P', new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*****",
+                "*",
+                "*",
+                "*"
+        });
+
+        patternMap.put('S', new String[]{
+                "*****",
+                "*",
+                "*",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        });
+
+        printBanner("OOPS", patternMap);
     }
 
-    public static void printWord(String word, List<CharacterPattern> list) 
-{
+    public static void printBanner(String word, Map<Character, String[]> 
+map) {
 
         for (int i = 0; i < 7; i++) {
 
             for (char c : word.toCharArray()) {
 
-                for (CharacterPattern cp : list) {
-
-                    if (cp.getLetter() == c) {
-                        System.out.print(cp.getPattern()[i] + "   ");
-                    }
+                String[] pattern = map.get(c);
+                if (pattern != null) {
+                    System.out.print(pattern[i] + "   ");
                 }
             }
             System.out.println();
         }
-    }
-
-    public static String[] getO() {
-        return new String[]{
-            "*****",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*****"
-        };
-    }
-
-    public static String[] getP() {
-        return new String[]{
-            "*****",
-            "*   *",
-            "*   *",
-            "*****",
-            "*",
-            "*",
-            "*"
-        };
-    }
-
-    public static String[] getS() {
-        return new String[]{
-            "*****",
-            "*",
-            "*",
-            "*****",
-            "    *",
-            "    *",
-            "*****"
-        };
     }
 }
